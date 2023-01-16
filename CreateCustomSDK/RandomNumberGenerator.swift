81fd031e89170e3fad52f84b2f35fa88221d3a67
NickSagan <9952728@gmail.com>
2023-01-16 16:05:15 +0100

    add class RandomNumberGenerator

---
+++ CreateCustomSDK/RandomNumberGenerator.swift
@@ -0,0 +1,27 @@
+//
+//  RandomNumberGenerator.swift
+//  CreateCustomSDK
+//
+//  Created by Nick Sagan on 16.01.2023.
+//
+
+import Foundation
+
+public class RandomNumberGenerator {
+    
+    private init(){}
+    
+    public static func getNumber() {
+        
+    }
+    
+    static func getNumberNotPublic() {
+        
+    }
+    
+    // MARK: - Private
+    
+    private func getRandomNumber() {
+        return
+    }
+}
